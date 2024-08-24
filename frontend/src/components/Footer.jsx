0c4f56d3d4535ed717f1b7ad0eb2ce2061e127ef
“rsushant583” <rsushant583@gmail.com>
2024-08-24 12:02:08 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed And Developed with ❤️ By Sushant Rai
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <p>Sushant ELITE  FITNESS</p>
+    </header>
+  );
+};
+
+export default Navbar;
